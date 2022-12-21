0fb50a2fa3afd0870d7d0d474e0d61ea38159f5a
GutoAS <agostinhosoberano2@gmail.com>
2022-12-21 12:31:39 +0200

    mortgage validation

--- src/Main.java
+++ src/Main.java
@@ -6,26 +6,47 @@ public class Main {
         final byte MONTHS_IN_YEAR = 12;
         final byte PERCENT = 100;
 
+        int amount = 0;
+        float monthlyInterest = 0;
+        int numbersOfPayments = 0;
+
        Scanner scanner= new Scanner(System.in);
 
-        System.out.print("Principal: ");
-        int amount = scanner.nextInt();
+        while (true) {
+            System.out.print("Principal (1K - 1M): ");
+            amount = scanner.nextInt();
+            if (amount >= 1000 && amount <= 1_000_000)
+                break;
+            System.out.println("Enter a number Between (1,000 and 1,000,000)");
+        }
 
-        System.out.print("Annual Interest Rate: ");
-        float annualInterest = scanner.nextFloat();
-        float monthlyInterest =  annualInterest / MONTHS_IN_YEAR / PERCENT ;
+        while (true) {
+            System.out.print("Annual Interest Rate: ");
+            float annualInterest = scanner.nextFloat();
+            if (annualInterest > 0 && annualInterest <= 30){
+                monthlyInterest = annualInterest / MONTHS_IN_YEAR / PERCENT;
+                break;
+            }
+                System.out.println("Enter a value greater than 0 and less than or equal to 30");
+        }
 
-        System.out.print("Period(years): ");
-        byte years = scanner.nextByte();
-        int numbersOfPayments = years * MONTHS_IN_YEAR;
+        while (true) {
+            System.out.print("Period(years): ");
+            byte years = scanner.nextByte();
+            if (years >= 1 && years <= 30){
+                numbersOfPayments = years * MONTHS_IN_YEAR;
+                break;
+            }
+                System.out.println("Enter a  value between 1 and 30");
 
-        System.out.print("Mortgage: ");
-        double mortgage = amount
-                * (monthlyInterest * Math.pow(1 + monthlyInterest, numbersOfPayments))
-                / (Math.pow(1 + monthlyInterest, numbersOfPayments) -1)
-                ;
-        String result = NumberFormat.getCurrencyInstance().format(mortgage);
-        System.out.print(result);
+        }
+
+            System.out.print("Mortgage: ");
+            double mortgage = amount
+                    * (monthlyInterest * Math.pow(1 + monthlyInterest, numbersOfPayments))
+                    / (Math.pow(1 + monthlyInterest, numbersOfPayments) - 1);
+            String result = NumberFormat.getCurrencyInstance().format(mortgage);
+            System.out.print(result);
 
 
     }
